a2d2319b2ba1b65b4f9c3d83cdafa5e816b06c76
admin <jack@lang-win.com.tw>
2017-06-08 14:42:33 +0800

    add mail list

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -713,6 +713,7 @@ class OrderController extends \yii\web\Controller
 		$mail->addAddress('Vivian.Li@padi.com.au');
 		$mail->addAddress('Stuart.Terrell@padi.com.au');
 		$mail->addAddress('mostafa.said@padi.com.au');
+		$mail->addAddress('ar@padi.com.au');
 //		$mail->addAddress('susan@lang-win.com.tw');
 //		$mail->addAddress('langchen@lang-win.com.tw');
 		if(!YII_ENV_DEV && $azure){
