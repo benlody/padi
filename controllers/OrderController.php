f016e0f990003374d3dcc262c79cb59d0c0ff176
admin <jack@lang-win.com.tw>
2019-08-13 11:40:15 +0800

    Revert "Change code 60020C in XM"
    This reverts commit 7874818263323cd8383f88c3ac496b2ecc303b3a.

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -1049,12 +1049,7 @@ class OrderController extends \yii\web\Controller
 				continue;
 			}
 
-//			$product_id =  $post_param[$product_idx];
-			if(0 == strcmp($post_param[$product_idx], "60020C") && $post_param['Order']['warehouse'] == 'xm'){
-				$product_id =  "60020C_XM";
-			} else {
-				$product_id =  $post_param[$product_idx];
-			}
+			$product_id =  $post_param[$product_idx];
 			$product_content[$product_id]['cnt'] = $product_cnt;
 			$product_content[$product_id]['done'] = false;
 			$weight = get_weight($product_id);
